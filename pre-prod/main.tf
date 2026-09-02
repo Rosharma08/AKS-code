e23f3542c6b3f13cdc6cd2b99706fc87302790da
Rohit Sharma <72161038+Rosharma08@users.noreply.github.com>
2026-09-02 22:24:48 +0530

    Update main.tf

--- pre-prod/main.tf
+++ pre-prod/main.tf
@@ -2,14 +2,4 @@ resource "azurerm_resource_group" "rg" {
   for_each = var.rgs
   name     = each.value.name
   location = each.value.location
-
-  tags = {
-    environment = "dev"
-    owner       = "Rohitsharma"
-    project     = "myproject"
-  }
-
-  lifecycle {
-    prevent_destroy = true
-  }
 }
